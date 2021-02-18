96fbc0d47d2c6a2c4e80ceb0f5f84563ced44ad3
Martina Šestak <martina.sestak@um.si>
2021-02-18 09:08:52 +0100

    Initial Java EE project structure

---
+++ src/main/java/um/si/de4a/HelloApplication.java
@@ -0,0 +1,9 @@
+package um.si.de4a;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
