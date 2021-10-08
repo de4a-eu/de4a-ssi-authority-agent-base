552643e300b6a3e4bec2e0e1b9c9d2809dd0fffb
martina.sestak <martina.sestak@um.si>
2021-10-08 14:17:22 +0200

    Added EBSI TIR integration. Release v1.0.

--- src/main/java/um/si/de4a/resources/ebsi/GetDIDEbsiResource.java
+++ src/main/java/um/si/de4a/resources/ebsi/GetDIDEbsiResource.java
@@ -1,25 +1,42 @@
 package um.si.de4a.resources.ebsi;
 
-
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import um.si.de4a.aries.AriesUtil;
 import um.si.de4a.db.DBUtil;
+import um.si.de4a.db.VCStatus;
+import um.si.de4a.db.VCStatusEnum;
+import um.si.de4a.util.DE4ALogger;
 
 import javax.ws.rs.*;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-@Path("/get-did")
+@Path("/get-did-ebsi")
 public class GetDIDEbsiResource {
-
     @GET
     @Consumes("application/json")
     @Produces("application/json")
-    public String getDID() throws IOException {
-        DBUtil dbUtil = new DBUtil();
-        String did = dbUtil.getDID();
+    public String getDID() throws IOException, ParseException {
+        Logger logger = DE4ALogger.getLogger();
+        LogRecord logRecordInfo = new LogRecord(Level.INFO, "");
+        LogRecord logRecordSevere = new LogRecord(Level.SEVERE, "");
 
-        if(did != null)
-            return did;
-        return "";
+        DBUtil dbUtil = new DBUtil();
+        String did = "";
+
+        try {
+            did = dbUtil.getDID();
+        }
+        catch(Exception ex){
+            logRecordSevere.setMessage("Error accessing data on Authority Agent DT.");
+            Object[] params = new Object[]{"Authority Agent DT", "Evidence portal DO", "1010"};
+            logRecordSevere.setParameters(params);
+            logger.log(logRecordSevere);
+        }
+
+        return did;
     }
 }
